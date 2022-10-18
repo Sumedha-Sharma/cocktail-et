12b7158c352bad0a40fb1e2e0962cb5ae0b1c987
Sumedha-Sharma <78554879+Sumedha-Sharma@users.noreply.github.com>
2022-10-18 20:18:19 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        This project is part of submission to the End Term Asssesment for Front End development.
+        <br></br>
+        Uses API  from The CocktailDB.com
+        <br></br>
+        Project submission brought to you by :
+        <br></br>
+        Sumedha Sharma 
+        <br></br>
+        Roll No: 2010991736
+        <br></br>
+        Group : G-16
+      </p>
+    </section>
+  );
+}
